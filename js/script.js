cdee55a24472baf58aeba732520cc7fed1baf8cd
Patrick Walczak <pwalczakx@gmail.com>
2022-06-19 21:19:45 +0200

    Adding counter

--- js/script.js
+++ js/script.js
@@ -10,4 +10,56 @@ import CompanyStrengths from "./companyStrengths.js";
 window.addEventListener("load", () => {
   document.querySelector("body").classList.remove("loading");
   document.querySelector(".loadingModal").classList.add("loaded");
+  updateWebCounter();
 });
+
+const loadingTimeLimitHandler = (milliseconds = 3000) => {
+  if (!isFinite(milliseconds)) milliseconds = 3000;
+
+  return new Promise((_, reject) =>
+    setTimeout(() => {
+      reject("Problem with internet connection");
+    }, milliseconds)
+  );
+};
+
+export const fetchHandler = (url, methodOptionsObject = {}) => {
+  return async () => {
+    try {
+      const response = await fetch(url, methodOptionsObject);
+
+      if (!response.ok) {
+        throw new Error([response.status, response.statusText, response.url]);
+      }
+      return response.json();
+    } catch (err) {
+      throw err;
+    }
+  };
+};
+
+const updateWebCounter = async () => {
+  try {
+    const webCounter = await Promise.race([
+      loadingTimeLimitHandler(),
+
+      fetchHandler(
+        "https://trading-platform-dabf0-default-rtdb.europe-west1.firebasedatabase.app/application/greenPowerCounter.json"
+      ),
+    ]);
+
+    await Promise.race([
+      loadingTimeLimitHandler(),
+
+      fetchHandler(
+        "https://trading-platform-dabf0-default-rtdb.europe-west1.firebasedatabase.app/application/greenPowerCounter.json",
+        {
+          method: "PATCH",
+          body: JSON.stringify(webCounter + 1),
+        }
+      ),
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+};
