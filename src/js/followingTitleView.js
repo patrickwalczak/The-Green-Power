9141e7cd61870bc91b9c737a2aac844f935afef8
Patrick Walczak <pwalczakx@gmail.com>
2025-02-26 21:02:31 +0100

    adjusting a highlights section to resolutions

--- src/js/followingTitleView.js
+++ src/js/followingTitleView.js
@@ -1,35 +1,15 @@
 class FollowingTitleView {
-  contentEl = document.querySelector(".content__block");
-  imageOne = document.querySelector(".article__image__2");
-  imageTwo = document.querySelector(".article__image__3");
-  imageThree = document.querySelector(".article__image__4");
-
-  fstTitle = `<h4 class="block__text">Biology <br/> Topics</h4>`;
-  sdTitle = `<h4 class="block__text">Environmental Protection Topics</h4>`;
-  thTitle = `<h4 class="block__text">Environmental technology Topics</h4>`;
-
-  images = document.querySelectorAll(".artImg");
-  sectionContainer = document.querySelector(".articles__section");
+  images = document.querySelectorAll('.artImg');
+  sectionContainer = document.querySelector('.articles__section');
   sectionObserver;
 
   constructor() {
-    [
-      [this.imageOne, "one", this.fstTitle],
-      [this.imageTwo, "two", this.sdTitle],
-      [this.imageThree, "three", this.thTitle],
-    ].forEach((arr) =>
-      arr[0].addEventListener(
-        "mouseenter",
-        this.generateMarkUp.bind(this, arr[1], arr[2])
-      )
-    );
-
     this.sectionObserver = new IntersectionObserver(
       this.addImagesActions.bind(this),
       {
         root: null,
         threshold: 0.1,
-        rootMargin: "150px",
+        rootMargin: '150px',
       }
     ).observe(this.sectionContainer);
   }
@@ -37,15 +17,9 @@ class FollowingTitleView {
   addImagesActions(entries, observer) {
     const [entry] = entries;
     if (entry.isIntersecting) {
-      this.images.forEach((img) => img.classList.add("loaded"));
+      this.images.forEach((img) => img.classList.add('loaded'));
       observer.unobserve(this.sectionContainer);
     }
-  }
-
-  generateMarkUp(id, content) {
-    this.contentEl.style.transform = `translateY(var(--translate-block-${id}))`;
-    this.contentEl.style.backgroundColor = `var(--block-${id}-color)`;
-    this.contentEl.innerHTML = content;
   }
 }
 
