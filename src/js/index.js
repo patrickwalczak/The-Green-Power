f3a4a774cc6eab9012f566fbd0e0d4391a2cd63b
Patrick Walczak <pwalczakx@gmail.com>
2025-02-27 08:44:07 +0100

    uncommenting js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-// import SideMenu from './sideMenu.js';
-// import HeroView from './heroView.js';
-// import EcoHomeView from './ecoHomeView.js';
-// import NavView from './navView.js';
+import SideMenu from './sideMenu.js';
+import HeroView from './heroView.js';
+import EcoHomeView from './ecoHomeView.js';
+import NavView from './navView.js';
 import SliderSectionView from './sliderSectionView.js';
 import FollowingTitleView from './followingTitleView.js';
-// import SolutionsView from './solutionsView.js';
-// import CompanyStrengths from './companyStrengths.js';
+import SolutionsView from './solutionsView.js';
+import CompanyStrengths from './companyStrengths.js';
 
 window.addEventListener('load', () => {
   document.querySelector('body').classList.remove('loading');
