9e3a16703c70c4e9223e58d63780072480a11ca2
Patrick Walczak <pwalczakx@gmail.com>
2025-04-14 18:34:29 +0200

    Fixing animation times in the Green Impact section, removing the highlights section

--- src/js/solutionsView.js
+++ src/js/solutionsView.js
@@ -1,22 +1,20 @@
 class SolutionsView {
 	sectionHeader = document.querySelector('.solutions__header_content');
-
 	headerSidebar = document.querySelector('.solutions__heading_sidebar');
-
 	sectionHeading = document.querySelector('.solutions__heading');
 
 	solutionOne = document.querySelector('#solution__content_1');
-
 	solutionTwo = document.querySelector('#solution__content_2');
-
 	solutionThree = document.querySelector('#solution__content_3');
-
 	solutionFour = document.querySelector('#solution__content_4');
 
 	solutionOneImg = document.querySelector('.solution__image_1');
 	solutionTwoImg = document.querySelector('.solution__image_2');
 	solutionThreeImg = document.querySelector('.solution__image_3');
 	solutionFourImg = document.querySelector('.solution__image_4');
+
+	scrollDownBtn = document.querySelector('.solutions__header_arrow_container');
+	solutionsHeading = document.querySelector('.solutions__section_heading');
 
 	threshold = [0, 0.1, 0.2, 0.4, 0.6, 1];
 
@@ -27,7 +25,11 @@ class SolutionsView {
 			threshold: [0.2, 1],
 		}).observe(this.sectionHeading);
 
-		window.addEventListener('scroll', this.transformOnScrollHandler.bind(this));
+		this.scrollDownBtn.addEventListener('click', () => {
+			const topPos = this.solutionsHeading.getBoundingClientRect().top + window.scrollY;
+
+			window.scrollTo({ top: topPos, behavior: 'smooth' });
+		});
 
 		[this.solutionOneImg, this.solutionTwoImg, this.solutionThreeImg, this.solutionFourImg].forEach((img) =>
 			this.attachObserver(img)
@@ -68,26 +70,8 @@ class SolutionsView {
 		this.sectionHeader.classList.add('introduce__heading');
 		this.headerSidebar.classList.add('introduce__sidebar');
 
-		observer.unobserve(this.sectionHeading);
+		observer.unobserve(entry.target);
 		this.sectionHeaderObserver = null;
-	}
-
-	transformOnScrollHandler() {
-		// if (window.scrollY > 770 && window.scrollY < 1450) {
-		// 	this.solutionOne.style.transform = `matrix(1, 0, 0, 1, 0, ${(1200 - window.scrollY) / 12})`;
-		// }
-		// if (window.scrollY > 1250 && window.scrollY < 2150) {
-		// 	this.solutionTwo.style.transform = `matrix(1, 0, 0, 1, 0, ${(1800 - window.scrollY) / 12})`;
-		// }
-		// if (window.scrollY > 1750 && window.scrollY < 2750) {
-		// 	this.solutionThree.style.transform = `matrix(1, 0, 0, 1, 0, ${(2200 - window.scrollY) / 12})`;
-		// }
-		// if (window.scrollY > 2200 && window.scrollY < 3200) {
-		// 	this.solutionFour.style.transform = `matrix(1, 0, 0, 1, 0, ${(2800 - window.scrollY) / 12})`;
-		// }
-		// if (window.scrollY > 200 && window.scrollY < 850) {
-		// 	this.sectionHeading.style.transform = `matrix(1, 0, 0, 1, 0, ${(500 - window.scrollY) / 10})`;
-		// }
 	}
 }
 export default new SolutionsView();
