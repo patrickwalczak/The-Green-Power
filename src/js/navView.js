ab753a9ccd2433d97b35d4cbfcf90364e7747557
Patrick Walczak <pwalczakx@gmail.com>
2025-04-14 19:04:51 +0200

    Removing redundant elements

--- src/js/navView.js
+++ src/js/navView.js
@@ -1,59 +1,50 @@
 class NavView {
-  donateBtn = null;
-  navTop = null;
-  navBottom = null;
-  navObserver = null;
-  nav = null;
+	donateBtn = null;
+	navTop = null;
+	navBottom = null;
+	navObserver = null;
+	nav = null;
 
-  constructor() {
-    this.selectElements();
-    this.attachEventListeners();
-    this.observeNavBottom();
-  }
+	constructor() {
+		this.selectElements();
+		this.attachEventListeners();
+		this.observeNavBottom();
+	}
 
-  selectElements() {
-    this.donateBtn = document.querySelector('.button__newsletter');
-    this.navTop = document.querySelector('.nav__top');
-    this.navBottom = document.querySelector('.nav__bottom');
-    this.nav = document.querySelector('nav');
-  }
+	selectElements() {
+		this.navTop = document.querySelector('.nav__top');
+		this.navBottom = document.querySelector('.nav__bottom');
+		this.nav = document.querySelector('nav');
+	}
 
-  attachEventListeners() {
-    this.navBottom.addEventListener('mouseover', (e) =>
-      this.handleHover(e, 0.5)
-    );
-    this.navBottom.addEventListener('mouseout', (e) => this.handleHover(e, 1));
-  }
+	attachEventListeners() {
+		this.navBottom.addEventListener('mouseover', (e) => this.handleHover(e, 0.5));
+		this.navBottom.addEventListener('mouseout', (e) => this.handleHover(e, 1));
+	}
 
-  observeNavBottom() {
-    this.navObserver = new IntersectionObserver(
-      (entries) => this.handleNavIntersection(entries),
-      { threshold: [0, 1] }
-    );
-    this.navObserver.observe(this.nav);
-  }
+	observeNavBottom() {
+		this.navObserver = new IntersectionObserver((entries) => this.handleNavIntersection(entries), {
+			threshold: [0, 1],
+		});
+		this.navObserver.observe(this.nav);
+	}
 
-  handleHover(e, opacity) {
-    const isNavLink = e.target.classList.contains('nav__link');
-    const isDonateBtn = e.target === this.donateBtn;
+	handleHover(e, opacity) {
+		const isNavLink = e.target.classList.contains('nav__link');
 
-    if (isNavLink || isDonateBtn) {
-      const links = this.navBottom.querySelectorAll('.nav__link');
-      links.forEach((el) => {
-        if (el !== e.target || isDonateBtn) el.style.opacity = opacity;
-      });
-      this.donateBtn.style.opacity = opacity;
-    }
-  }
+		if (isNavLink) {
+			const links = this.navBottom.querySelectorAll('.nav__link');
+			links.forEach((el) => {
+				if (el !== e.target) el.style.opacity = opacity;
+			});
+		}
+	}
 
-  handleNavIntersection(entries) {
-    entries.forEach((entry) => {
-      this.navTop.classList.toggle(
-        'fixed__nav',
-        !(entry.intersectionRatio > 0.8 && entry.isIntersecting)
-      );
-    });
-  }
+	handleNavIntersection(entries) {
+		entries.forEach((entry) => {
+			this.navTop.classList.toggle('fixed__nav', !(entry.intersectionRatio > 0.8 && entry.isIntersecting));
+		});
+	}
 }
 
 export default new NavView();
